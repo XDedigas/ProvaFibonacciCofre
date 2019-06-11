4ee8f6440933fc0715cd6431ff6cb534e9f75967
XDedigas <edgarfelipeventurini@gmail.com>
2019-06-11 10:08:46 -0300

    Add files via upload

---
+++ FibonacciCofre/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FibonacciCofre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbPista = new System.Windows.Forms.Label();
+            this.tbPista = new System.Windows.Forms.TextBox();
+            this.btConfirmarCodigo = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.stripTempo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbNovaPista = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbCodigo = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbPista
+            // 
+            this.lbPista.AutoSize = true;
+            this.lbPista.Location = new System.Drawing.Point(12, 18);
+            this.lbPista.Name = "lbPista";
+            this.lbPista.Size = new System.Drawing.Size(36, 13);
+            this.lbPista.TabIndex = 0;
+            this.lbPista.Text = "Pista: ";
+            // 
+            // tbPista
+            // 
+            this.tbPista.Location = new System.Drawing.Point(54, 15);
+            this.tbPista.Name = "tbPista";
+            this.tbPista.Size = new System.Drawing.Size(155, 20);
+            this.tbPista.TabIndex = 1;
+            this.tbPista.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbPista_KeyPress);
+            // 
+            // btConfirmarCodigo
+            // 
+            this.btConfirmarCodigo.Location = new System.Drawing.Point(54, 41);
+            this.btConfirmarCodigo.Name = "btConfirmarCodigo";
+            this.btConfirmarCodigo.Size = new System.Drawing.Size(155, 23);
+            this.btConfirmarCodigo.TabIndex = 2;
+            this.btConfirmarCodigo.Text = "Confirmar código";
+            this.btConfirmarCodigo.UseVisualStyleBackColor = true;
+            this.btConfirmarCodigo.Click += new System.EventHandler(this.btConfirmarCodigo_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.stripTempo});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 120);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(221, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(50, 17);
+            this.toolStripStatusLabel1.Text = "Tempo: ";
+            // 
+            // stripTempo
+            // 
+            this.stripTempo.Name = "stripTempo";
+            this.stripTempo.Size = new System.Drawing.Size(0, 17);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nova Pista:";
+            // 
+            // lbNovaPista
+            // 
+            this.lbNovaPista.AutoSize = true;
+            this.lbNovaPista.Location = new System.Drawing.Point(91, 74);
+            this.lbNovaPista.Name = "lbNovaPista";
+            this.lbNovaPista.Size = new System.Drawing.Size(0, 13);
+            this.lbNovaPista.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Código: ";
+            // 
+            // lbCodigo
+            // 
+            this.lbCodigo.AutoSize = true;
+            this.lbCodigo.Location = new System.Drawing.Point(72, 97);
+            this.lbCodigo.Name = "lbCodigo";
+            this.lbCodigo.Size = new System.Drawing.Size(0, 13);
+            this.lbCodigo.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 142);
+            this.Controls.Add(this.lbCodigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbNovaPista);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btConfirmarCodigo);
+            this.Controls.Add(this.tbPista);
+            this.Controls.Add(this.lbPista);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cofre Fibonacci";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbPista;
+        private System.Windows.Forms.TextBox tbPista;
+        private System.Windows.Forms.Button btConfirmarCodigo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel stripTempo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbNovaPista;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbCodigo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FibonacciCofre/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FibonacciCofre
+{
+    public partial class Form1 : Form
+    {
+        #region Variaveis
+        private SortedList<long, long> ListaFibonacci = null;
+        private SortedList<long, long> ListaCodigos = null;
+        private List<long> ListaPistas = null;
+        private StringBuilder sbTextoFinal = null;
+        private Random randNum = null;
+        private string ErroAo = "";
+        private int index = 0;
+        #endregion
+
+        #region Construtor
+        public Form1()
+        {
+            InitializeComponent();
+            ListaFibonacci = new SortedList<long, long>();
+            InicializaVariaveis();
+            CarregarListaFibonacci();
+            GerarCabecalhoTextoFinal();
+        }
+        #endregion
+
+        private void btConfirmarCodigo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ErroAo = "Converter a informação digitada no campo 'Pista'";
+                if (Convert.ToInt32(tbPista.Text) > 78 || Convert.ToInt32(tbPista.Text) < 0)
+                {
+                    MessageBox.Show("A pista informada é inválida!");
+                }
+                else
+                {
+                    if ((lbNovaPista.Text != tbPista.Text && index == 0) || lbNovaPista.Text == tbPista.Text)
+                    {
+                        DateTime inicio = DateTime.Now;
+                        ErroAo = "Buscar a informação na lista de Fibonacci";
+                        lbCodigo.Text = ListaFibonacci[Convert.ToInt32(tbPista.Text)].ToString("0000000000000000");
+                        long random = randNum.Next(0, 78);
+                        AddNovaPista(random);
+                        lbNovaPista.Text = ListaPistas[index].ToString();
+                        ListaCodigos.Add(Convert.ToInt64(lbNovaPista.Text), Convert.ToInt64(lbCodigo.Text));
+                        GerarTextoFinal(tbPista.Text, lbCodigo.Text, (DateTime.Now - inicio).ToString());
+                        tbPista.Text = "";
+                        if (ListaCodigos.Count == 7)
+                        {
+                            sbTextoFinal.AppendLine();
+                            sbTextoFinal.AppendLine("=======================================");
+                            MessageBox.Show(sbTextoFinal.ToString());
+                            InicializaVariaveis();
+                        }
+                        else
+                        {
+                            index++;
+                        }
+                        tbPista.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("A pista já foi informada anteriormente. Favor informar a nova pista informada!");
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erro ao: " + ErroAo + "\r\nErro: " + err.Message);
+            }
+        }
+
+        public void CarregarListaFibonacci()
+        {
+            try
+            {
+                ErroAo = "Carregar a lista Fibonacci";
+                long a = 0; long b = 1; long aux = 0;
+                ListaFibonacci.Add(a, 0);
+                ListaFibonacci.Add(b, 1);
+                for (int i = 2; i < 79; i++)
+                {
+                    ListaFibonacci.Add(i, a + b);
+                    aux = a;
+                    a = b;
+                    b = a + aux;
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erro ao: " + ErroAo + "\r\nErro: " + err.Message);
+            }
+        }
+
+        public void InicializaVariaveis()
+        {
+            ListaCodigos = new SortedList<long, long>();
+            ListaPistas = new List<long>();
+            sbTextoFinal = new StringBuilder();
+            randNum = new Random();
+            index = 0;
+            tbPista.Text = "";
+            lbNovaPista.Text = "";
+            lbCodigo.Text = "";
+        }
+
+        public void AddNovaPista(long pista)
+        {
+            while (ListaPistas.Contains(pista))
+            {
+                pista = randNum.Next(0, 78);
+            }
+            ListaPistas.Add(pista);
+        }
+
+        public void GerarCabecalhoTextoFinal()
+        {
+            sbTextoFinal = new StringBuilder();
+            sbTextoFinal.AppendLine("Foram realizados todas as buscas dos códigos. Segue abaixo as informações necessárias para abrir o cofre:");
+            sbTextoFinal.AppendLine();
+            sbTextoFinal.AppendLine("============Códigos gerados============");
+        }
+
+        public void GerarTextoFinal(string pista, string cod, string tempo)
+        {
+            sbTextoFinal.AppendLine($"Pista: {pista} | Código: {cod} | Tempo de consulta: {tempo}");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            stripTempo.Text = DateTime.Now.ToString();
+        }
+
+        private void tbPista_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                btConfirmarCodigo.PerformClick();
+            }
+        }
+    }
+}
